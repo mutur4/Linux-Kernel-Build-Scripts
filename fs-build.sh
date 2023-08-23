e1aa86d7b601c89852a509ed43242798a6a361ac
BinaryChunk <8421gilbertrobert@gmail.com>
2023-08-23 11:52:20 +0300

    lol

--- fs-build.sh
+++ fs-build.sh
@@ -3,11 +3,10 @@
 BUSYBOX_VERSION=1.32.0
 
 init(){
+        printf "[+] DownLoading BusyBox!\n"
         wget -q -c https://busybox.net/downloads/busybox-$BUSYBOX_VERSION.tar.bz2
         [ -e busybox-$BUSYBOX_VERSION.tar.bz2 ] && tar xjf busybox-$BUSYBOX_VERSION.tar.bz2
 
-
-        printf "BB Build...\n"
 
         make -C busybox-$BUSYBOX_VERSION defconfig
         sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/g' busybox-$BUSYBOX_VERSION/.config
@@ -15,16 +14,10 @@ init(){
         make -C busybox-$BUSYBOX_VERSION -j16
         make -C busybox-$BUSYBOX_VERSION install
 
-        printf "Fs Buildz..\n";
+        printf "[+] Building the FileSystem\n";
 
         mkdir -p ./rootfs/{bin,sbin,etc,proc,sys,usr/bin,root,home/ffs}
         cp -ar ./busybox-$BUSYBOX_VERSION/_install/* rootfs
-
-        echo "Building Modules..\n"
-
-        [ -e vuln.ko ] || make
-
-        cp vuln.ko rootfs
 }
 
 init
